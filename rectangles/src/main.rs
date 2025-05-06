b3fef6cff791caab5a4fcf1b9262db00f42e7eed
Lars Grothe <lars.grothe@student.hs-rm.de>
2025-05-06 22:05:13 +0200

    feat: The code now uses structs instead of tuples. Additionally the rectangle implementation now contains to methods area and can_hold.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,12 +1,49 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width >= rect.width && self.height >= rect.height
+    }
+}
+
 fn main() {
-    let rect1 = (30, 50);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    let square1 = Rectangle::square(3);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(rect1)
+        rect1.area()
     );
-}
-
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+    println!(
+        "The area of the square is {} square pixels.",
+        square1.area()
+    );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
