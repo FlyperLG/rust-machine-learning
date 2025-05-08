62705a0e82c2e72d2c1469f20c4b48c871ee6c0e
Lars Grothe <lars.grothe@student.hs-rm.de>
2025-05-08 17:13:35 +0200

    feat: implemented the base tic-tac-toe game logic.

--- tic_tac_toe/src/main.rs
+++ tic_tac_toe/src/main.rs
@@ -15,39 +15,144 @@ impl Grid {
         }
     }
 
-    fn set_value(&mut self, row: usize, col: usize, char: char) {
+    fn set_value(&mut self, row: usize, col: usize, char: char) -> bool {
+        if self.grid[row][col] != '-' {
+            return false;
+        }
         self.grid[row][col] = char;
+        return true;
+    }
+
+    fn check_game_over(&self) -> bool {
+        for row in self.grid {
+            let mut has_row: bool = true;
+            let mut row_value: char = '-';
+            for value in row {
+                if value == '-' {
+                    has_row = false;
+                    break;
+                }
+
+                if value != row_value && row_value != '-' {
+                    has_row = false;
+                    break;
+                }
+
+                row_value = value;
+            }
+
+            if has_row {
+                return true;
+            }
+        }
+
+        for col_index in 0..3 {
+            let mut has_col: bool = true;
+            let mut col_value: char = '-';
+            for row_index in 0..3 {
+                let value = self.grid[row_index][col_index];
+                if value == '-' {
+                    has_col = false;
+                    break;
+                }
+
+                if value != col_value && col_value != '-' {
+                    has_col = false;
+                    break;
+                }
+
+                col_value = value;
+            }
+            if has_col {
+                return true;
+            }
+        }
+
+        let mut has_diagonal: bool = true;
+        let mut diagonal_value: char = '-';
+        for diagonal_index in 0..3 {
+            let value = self.grid[diagonal_index][diagonal_index];
+            if value == '-' {
+                has_diagonal = false;
+                break;
+            }
+
+            if value != diagonal_value && diagonal_value != '-' {
+                has_diagonal = false;
+                break;
+            }
+
+            diagonal_value = value;
+        }
+        if has_diagonal {
+            return true;
+        }
+
+        let mut has_diagonal: bool = true;
+        let mut diagonal_value: char = '-';
+        for diagonal_index in 0..3 {
+            let value = self.grid[0 + diagonal_index][2 - diagonal_index];
+            if value == '-' {
+                has_diagonal = false;
+                break;
+            }
+
+            if value != diagonal_value && diagonal_value != '-' {
+                has_diagonal = false;
+                break;
+            }
+
+            diagonal_value = value;
+        }
+        if has_diagonal {
+            return true;
+        }
+        return false;
     }
 }
 
-struct Player {
-    name: String,
-    display_char: char,
+enum Player {
+    ConsolePlayer { name: String, display_char: char },
 }
 
-struct InputManager;
-impl InputManager {
-    fn get_position(player: &Player) -> Option<(usize, usize)> {
-        println!("{} please select a row by typing 1/2/3.", player.name);
-        let mut row: String = String::new();
-        io::stdin()
-            .read_line(&mut row)
-            .expect("Failed to read line");
-        let row: usize = match row.trim().parse::<usize>() {
-            Ok(num) => num - 1,
-            Err(_) => return None,
-        };
+impl Player {
+    fn get_position(&self) -> Option<(usize, usize)> {
+        match self {
+            Self::ConsolePlayer { name, .. } => {
+                println!("{} please select a row by typing 1/2/3.", name);
+                let mut row: String = String::new();
+                io::stdin()
+                    .read_line(&mut row)
+                    .expect("Failed to read line");
+                let row: usize = match row.trim().parse::<usize>() {
+                    Ok(num) => num - 1,
+                    Err(_) => return None,
+                };
 
-        println!("{} please select a col by typing 1/2/3.", player.name);
-        let mut col: String = String::new();
-        io::stdin()
-            .read_line(&mut col)
-            .expect("Failed to read line");
-        let col: usize = match col.trim().parse::<usize>() {
-            Ok(num) => num - 1,
-            Err(_) => return None,
+                println!("{} please select a col by typing 1/2/3.", name);
+                let mut col: String = String::new();
+                io::stdin()
+                    .read_line(&mut col)
+                    .expect("Failed to read line");
+                let col: usize = match col.trim().parse::<usize>() {
+                    Ok(num) => num - 1,
+                    Err(_) => return None,
+                };
+                return Some((row, col));
+            }
         };
-        return Some((row, col));
+    }
+
+    fn symbol(&self) -> char {
+        match self {
+            Self::ConsolePlayer { display_char, .. } => *display_char,
+        }
+    }
+
+    fn name(&self) -> &str {
+        match self {
+            Self::ConsolePlayer { name, .. } => name,
+        }
     }
 }
 
@@ -55,16 +160,37 @@ fn main() {
     let mut grid = Grid {
         grid: [['-'; 3]; 3],
     };
-    let player = Player {
-        name: String::from("Player1"),
-        display_char: 'X',
-    };
 
+    let players = [
+        Player::ConsolePlayer {
+            name: String::from("Player1"),
+            display_char: 'X',
+        },
+        Player::ConsolePlayer {
+            name: String::from("Player2"),
+            display_char: 'O',
+        },
+    ];
+
+    let mut player_index = 0;
     loop {
         grid.print();
-        match InputManager::get_position(&player) {
-            Some((row, col)) => grid.set_value(row, col, player.display_char),
-            None => continue,
+        let player = &players[player_index];
+        match player {
+            Player::ConsolePlayer { display_char, .. } => {
+                if let Some((row, col)) = player.get_position() {
+                    grid.set_value(row, col, *display_char);
+                }
+            }
         }
+
+        let is_game_over = grid.check_game_over();
+        if is_game_over {
+            grid.print();
+            println!("{} has won the game.", player.name());
+            break;
+        }
+        player_index += 1;
+        player_index = player_index % 2;
     }
 }
